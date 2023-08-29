742d79985b724e2ef66b90f5f57622691ccb8092
Charles Levos <crcny02@aol.com>
2023-08-29 18:24:03 -0400

    commented the balloons program

--- src/npw/Balloons.java
+++ src/npw/Balloons.java
@@ -27,18 +27,28 @@ public class Balloons {
     // THE PAINTER DOING ITS THING
 
     private void paintTheImage() {
+        // creates a canvas with a height of 600 and width of 800 px
         SPainter painter = new SPainter("Balloons", 800, 600);
+        // colors the canvas the blue
         paintSky(painter);
         int nrOfBalloons = 200;
+        // create 200 randomly sized and colored balloons on the canvas
         paintBalloons(painter, nrOfBalloons);
     }
 
     private void paintSky(SPainter painter) {
         painter.setColor(Color.BLUE);
+        // The sky is a rectangle object
         SRectangle sky = new SRectangle(2000,2800);
+        // Put the blue rectangle on the canvas to represent the sky
         painter.paint(sky);
     }
 
+    /**
+     * paint randomly colored sized balloons
+     * @param painter
+     * @param nrOfBalloons represents the number of balloons
+     */
     private void paintBalloons(SPainter painter, int nrOfBalloons) {
         int i = 1;
         while(i <= nrOfBalloons) {
@@ -47,7 +57,12 @@ public class Balloons {
         }
     }
 
+    /**
+     * paint one balloon on the canvas
+     * @param painter
+     */
     private void paintOneBalloon(SPainter painter) {
+        //Get a random number generator
         Random rgen = new Random();
         int rn = rgen.nextInt(4);
         if (rn == 0) {
@@ -60,13 +75,19 @@ public class Balloons {
         else  {
             painter.setColor(Color.GREEN);
         }
+        //Tells the painter to change location
         painter.move();
+        //Determines the radius of the circle randomly
         int n = rgen.nextInt(4);
         n = n +1;
         int balloonRadius = 10*n ;
+        // Create the circle object
         SCircle balloon = new SCircle(balloonRadius);
+        // Paints the circle on the canvas
         painter.paint(balloon);
+        // sets color to black
         painter.setColor(Color.BLACK);
+        // draws a black circle around the circles
         painter.draw(balloon);
     }
 
