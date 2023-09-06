3fd0b11fbe9e6a71886b3aa5f856ae6f5fed273f
Charles Levos <crcny02@aol.com>
2023-09-06 11:46:15 -0400

    lab4 task3

---
+++ src/expressions/ExpressionsThing.java
@@ -0,0 +1,17 @@
+package expressions;
+
+/**
+ * This program affords opportunities to explore the construction of arithmetic expressions in the context of some very simple
+ * problem solving.
+ */
+public class ExpressionsThing {
+    public static void main(String[] args){
+        double one= 3.14 * 5+5;
+        System.out.println("one = " + one);
+        double two= 3.14 *(5+5);
+        System.out.println("two = "+two);
+        double three=(3.14*(5+5));
+        System.out.println("three = "+three);
+    }
+
+}
