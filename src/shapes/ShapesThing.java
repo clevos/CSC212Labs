d83260e37580cf3aa240be744f39f67b6bf55f3c
Charles Levos <crcny02@aol.com>
2023-09-11 15:23:58 -0400

    Lab4 task 8

---
+++ src/shapes/ShapesThing.java
@@ -0,0 +1,15 @@
+package shapes;
+/*
+* The program affords opportunities to explore the computational solution to geometric problems
+* bind the construction and use of basic shapes
+ */
+public class ShapesThing {
+    public static  void main(String[] args){
+    SSquare square=new SSquare(400);
+    // The toString method creates a text representation of the object
+    System.out.println("square = " + square.toString());
+    System.out.println("area of square = " +square.area());
+    System.out.println("perimeter of square= " + square.perimeter());
+    System.out.println("diagonal of square " + square.diagonal());
+    }
+}
